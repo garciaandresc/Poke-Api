922c694cc912ded0f644e0be8d4916274b1cff7e
AndresGarcia <garciaandresc@gmail.com>
2023-07-05 14:46:48 -0600

    gradient

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   }, []);
 
   return (
-    <main className="w-screen min-h-screen bg-gradient-to-r from-orange-200 to-red-600 flex flex-col gap-10 p-10">
+    <main className="w-screen min-h-screen bg-gradient-to-br from-orange-200 to-red-700 flex flex-col gap-10 p-10">
       <header className="w-screen flex justify-center pt-10">
         <div className="flex flex-col justify-center text-center">
           <img
