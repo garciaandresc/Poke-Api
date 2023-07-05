c42515097b73307df84ca63b16965be84a66f098
AndresGarcia <garciaandresc@gmail.com>
2023-07-04 23:43:50 -0600

    First commit PokeDex

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import PokemonCard from "./Components/pokemonCard";
+
+function App() {
+  const [pokemons, setPokemons] = useState<any>([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=21")
+      .then((response) => response.json())
+      .then((response) => {
+        setPokemons(response.results);
+      });
+  }, []);
+
+  return (
+    <main className="w-screen min-h-screen bg-slate-200 flex flex-col gap-10 p-10">
+      <header className="w-screen flex justify-center pt-10">
+        <div className="flex flex-col justify-center border-4 text-center">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+            alt="pokeapi"
+          />
+          <h1 className="text-lg font-semibold p-4">Pokémon</h1>
+        </div>
+      </header>
+      <section className="grid grid-cols-7 gap-4">
+        {pokemons.map((pokemon) => {
+          return (
+            <PokemonCard
+              key={pokemon.url}
+              name={pokemon.name}
+              url={pokemon.url}
+            />
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/pokemonCard.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+interface Props {
+  name: string;
+  url: string;
+}
+
+export default function PokemonCard(props: Props) {
+  const [pokemons, setPokemon] = useState<any>({});
+  useEffect(() => {
+    fetch(props.url)
+      .then((response) => response.json())
+      .then((response) => {
+        setPokemon(response);
+      });
+  }, [props.url]);
+  return (
+    <article className="bg-black/10 p-4 flex flex-col justify-center items-center rounded-md hover:bg-slate-300 cursor-pointer">
+      <img src={pokemons?.sprites?.front_default} alt={props.name} />
+      <h2 className="text-white font-semibold text-lg">{props.name}</h2>
+    </article>
+  );
+}
