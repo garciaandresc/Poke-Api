df30500936895dfa7983063209d26569abcf8373
AndresGarcia <garciaandresc@gmail.com>
2023-07-05 14:32:06 -0600

    color

--- src/Components/pokemonCard.tsx
+++ src/Components/pokemonCard.tsx
@@ -15,9 +15,12 @@ export default function PokemonCard(props: Props) {
       });
   }, [props.url]);
   return (
-    <article className="bg-black/10 p-4 flex flex-col justify-center items-center rounded-md hover:bg-slate-300 cursor-pointer">
+    <article className="bg-black/10 p-4 flex flex-col justify-center items-center rounded-md hover:bg-orange-200 cursor-pointer">
       <img src={pokemons?.sprites?.front_default} alt={props.name} />
       <h2 className="text-white font-semibold text-lg">{props.name}</h2>
+      <div className="rounded-lg border-2 p-1 m-1">
+        {pokemons?.types?.type?.name}
+      </div>
     </article>
   );
 }
